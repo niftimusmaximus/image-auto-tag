11b3c1b8c003ad1ce5f81163c2e3485454bd3c17
David Hamilton <david@david-linux-pc.site>
2016-10-19 00:18:57 +1100

    Updated with confidence level parameters

--- image-auto-tag.py
+++ image-auto-tag.py
@@ -6,26 +6,47 @@ import argparse
 import json
 import sys
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import PIL
+from PIL import Image
+import io
 
 parser = argparse.ArgumentParser(description='Add XMP metadata based on Azure Computer Vision')
 parser.add_argument('--key', nargs='?',required=True, help='Azure Computer Vision API key')
-parser.add_argument('--confidenceLevel', help='Confidence level for adding tags',type=float, default=0.9)
+parser.add_argument('--tagConfidenceLevel', help='Confidence level for adding tags',type=float, default=0.9)
+parser.add_argument('--descriptionConfidenceLevel', help='Confidence level for adding description',type=float, default=0.9)
+parser.add_argument('--categoryConfidenceLevel', help='Confidence level for adding category',type=float, default=0.9)
+parser.add_argument('--azureResizeWidth', required=False, help='Temporarily resize to <azureResizeWidth> before uploading to Azure', type=int, default=800)
 parser.add_argument('inputFiles', help='Input file list', type=argparse.FileType('rb'), nargs='+')
 args = parser.parse_args()
 
 
-p_confidence_level=args.confidenceLevel;
+p_tag_confidence_level=args.tagConfidenceLevel;
+p_description_confidence_level=args.descriptionConfidenceLevel;
+p_category_confidence_level=args.categoryConfidenceLevel;
+p_azure_resize_width=args.azureResizeWidth;
 print(args.inputFiles)
 
 v_tags=[]
 
 for v_input_file in args.inputFiles:   
 
-    try:
-        byte = v_input_file.read()   
+    try:       
+        byteio = io.BytesIO()        
+        img = Image.open(v_input_file.name)
+        wpercent = (p_azure_resize_width / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((p_azure_resize_width, hsize), PIL.Image.ANTIALIAS)
+        img.save(byteio, format="JPEG")
+        byteio.seek(0)
+        byte=byteio.read()
+        img.close()
+        byteio.close()
         print(len(byte))
     finally:
         v_input_file.close()
+
+
+
 
     headers = {
         # Request headers
@@ -65,7 +86,7 @@ for v_input_file in args.inputFiles:
     xmp.set_property('http://www.digikam.org/ns/1.0/', u'TagsList', v_json_result['categories'][0]['name'])
     
     for v_tag in v_json_result['tags']:
-        if (v_tag['confidence']>=p_confidence_level):           
+        if (v_tag['confidence']>=p_tag_confidence_level):           
             xmp.append_array_item(consts.XMP_NS_DC, u'Subject', v_tag['name'], {'prop_array_is_ordered': True, 'prop_value_is_array': True} )
     v_tags=[]
     if xmpfile.can_put_xmp(xmp):
